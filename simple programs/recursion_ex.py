2f0d8f6df6125dbbf38c6e711cd06f83bb832398
AdmiralAnne <81945731+AdmiralAnne@users.noreply.github.com>
2021-05-28 04:44:36 +0000

    Add files via upload

---
+++ simple programs/recursion_ex.py
@@ -0,0 +1,20 @@
+# check wheter a number is prime or not
+# prime_number --- divisible only by 1 and the number itself
+# imp: not divisible by any number between 2 --- "num" (it is a prime number)
+
+def prime():
+    num=int(input("Enter the number you want to check: "))
+    val=True
+    if (num==1 or num==0):
+        val=False
+    elif num>1:
+        for i in range(2,num):
+            if(num % i)==0:
+                val=False
+                break
+    if val==True:
+        print("It is a prime number")
+    elif val==False:
+        print("It is not a prime number")
+    prime()
+prime()
